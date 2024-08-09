7abf9cadbe2841c6ab41808704fed9e45bb7eb4f
Arjin <arjin.ferman@proton.me>
2024-08-09 21:45:01 +0200

    [#19] Populate map from data files instead of LUA

--- core/data/scripts/files/world_files/system_file.lua
+++ core/data/scripts/files/world_files/system_file.lua
@@ -1,8 +1,11 @@
 local Class       = require("class")
 local Inspect     = require("inspect")
 local Json        = require("dkjson")
+local Ship        = require("ship")
+local ShipGroup   = require("ship_group")
 local StarSystem  = require('star_system')
 local Utils       = require('utils')
+local Wing        = require("wing")
 
 local SystemFile = Class()
 
@@ -19,7 +22,55 @@ function SystemFile:loadSystem(filename)
 
     system_file:close()
 
-    return StarSystem(system_data.StarSystem)
+    local system = StarSystem(system_data.StarSystem)
+    self:loadShips(system, system_data.Ships)
+
+    return system
+end
+
+local ship_init_func = function(data) return Ship(data) end
+
+local ship_initializers = {
+    ["Ship"] = ship_init_func,
+    ["Fighter"] = ship_init_func,
+    ["Bomber"] = ship_init_func,
+    ["Transport"] = ship_init_func,
+    ["Cruiser"] = ship_init_func,
+    ["Capital"] = ship_init_func,
+    ["Group"] = function(data)
+        local ships = {}
+        for _, ship_data in pairs(data.Ships) do
+            table.insert(ships, ship_init_func(ship_data))
+        end
+
+        data.Ships = ships
+        return ShipGroup(data)
+    end,
+    ["Wing"] = function(data)
+        local ships = {}
+        for _, ship_data in pairs(data.Ships) do
+            table.insert(ships, ship_init_func(ship_data))
+        end
+
+        data.Ships = ships
+        return Wing(data)
+    end
+}
+
+function SystemFile:loadShips(system, ships_data)
+
+    for _, ship_data in pairs(ships_data) do
+        ba.println("Adding: " .. Inspect({ ship_data.Name }))
+        local ship = ship_initializers[ship_data.Type](ship_data)
+
+        if ship.Type == "Group" and ship:getTopShip().Name == "Taganrog" then
+            ship.SemiMajorAxis = Utils.Math.AU * 50
+            ship.MeanAnomalyEpoch = math.random() * 360
+            system:get("Sol"):add(ship)
+        else
+            GameMapGenerator.addShipToRandomOrbit(ship, system)
+        end
+    end
 end
 
 return SystemFile
